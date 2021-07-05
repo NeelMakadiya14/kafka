9f242c09f4c1f3ec8588e966fe4c75f42935c68d
NeelMakadiya14 <nmakadiya1@gmail.com>
2021-07-05 22:47:55 +0530

    Added Java files for Backup and Retrieve

---
+++ core/src/main/java/Backup/Config.java
@@ -0,0 +1,13 @@
+package Backup;
+
+public class Config {
+    public static String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+
+    public static String ACCESS_KEY = "";
+
+    public static String SECRET_KEY = "";
+
+    public static String BUCKET = "";
+
+    public static String REGION = "";
+}
